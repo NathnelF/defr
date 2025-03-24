eb1cacd871248b1f227e3fa7fa31cca3589b28d6
Nate Fuller <natefuller8@gmail.com>
2025-03-24 11:43:51 -0400

    changed create contract to use names instead of ids and added functionality to create customer / services upon entry

--- defr/contracts.py
+++ defr/contracts.py
@@ -1,11 +1,15 @@
 from flask import(
     Blueprint, flash, redirect, render_template, request, url_for
 )
-from .tables import db, Contract  # Import the db and models
+from .tables import db, Contract, Customer, Service  # Import the db and models
 from .auth import login_required
 from datetime import datetime
 
 bp = Blueprint('contracts', __name__)
+
+@bp.route('/')
+def direct_to_login():
+    return redirect(url_for('auth.login'))
 
 @bp.route('/index')
 @login_required
@@ -20,8 +24,8 @@ def index():
 @login_required
 def create():
     if request.method == 'POST':
-        customer_id = request.form['customer_id']
-        service_id = request.form['service_id']
+        customer_name = request.form['customer_name']
+        service_name = request.form['service_name']
         term = request.form['term']
         annual_amount = request.form['annual_amount']
         original_start = request.form['original_start']
@@ -31,10 +35,10 @@ def create():
         price_increase = request.form['price_increase']
         error = None
 
-        if not customer_id:
+        if not customer_name:
             error = 'Customer is required'
 
-        elif not service_id:
+        elif not service_name:
             error = 'Service is required'
 
         elif not annual_amount:
@@ -42,6 +46,28 @@ def create():
         
         elif not term:
             error = 'Term is required'
+
+        #lookup customer id from name
+        customer = Customer.query.filter(Customer.customer_name == customer_name).first()
+        if customer:
+            customer_id = customer.customer_id
+        else:
+            new_customer = Customer(customer_name=customer_name)
+            db.session.add(new_customer)
+            db.session.commit()
+            customer = Customer.query.filter(Customer.customer_name == customer_name).first()
+            customer_id = customer.customer_id
+        
+        #lookup service id from name
+        service = Service.query.filter(Service.service_name == service_name).first()
+        if service:
+            service_id = service.service_id
+        else:
+            new_service = Service(service_name=service_name)
+            db.session.add(new_service)
+            db.session.commit()
+            service = Service.query.filter(Service.service_name == service_name).first()
+            service_id = service.service_id
         
         if error is not None:
             flash(error)
@@ -77,7 +103,7 @@ def edit(contract_id):
         original_start = request.form['original_start']
         current_start = request.form['current_start']
         current_end = request.form['current_end']
-        auto_renew = 'auto_renew' in request.form  # Checkbox returns 'on' if checked
+        auto_renew = 'auto_renew' in request.form 
         price_increase = request.form['price_increase']
         error = None
 
@@ -96,17 +122,15 @@ def edit(contract_id):
         if error is not None:
             flash(error)
         else:
-            new_contract = Contract(
-                customer_id=customer_id,
-                service_id=service_id,
-                term=term,
-                annual_amount=annual_amount,
-                original_start=original_start,
-                current_start=current_start,
-                current_end=current_end,
-                auto_renew=auto_renew,
-                price_increase=price_increase)
-            db.session.add(new_contract)
+            contract.customer_id=customer_id
+            contract.service_id=service_id
+            contract.term=term
+            contract.annual_amount=annual_amount
+            contract.original_start=original_start
+            contract.current_start=current_start
+            contract.current_end=current_end
+            contract.auto_renew=auto_renew
+            contract.price_increase=price_increase
             db.session.commit()
 
             flash('Created contract successfully')
