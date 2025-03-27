a31c4793e157c7c94a7b1e838edf51812e98ab65
Nate Fuller <natefuller8@gmail.com>
2025-03-27 12:42:08 -0400

    Added lifecycle and qb event tables

--- defr/tables.py
+++ defr/tables.py
@@ -67,3 +67,40 @@ class Schedule(db.Model):
     def __repr__(self):
         return f'<Event id: {self.event_id}, Contract id: {self.contract_id}, Customer id: {self.customer_id}, Service id: {self.service_id}>'
     
+class Lifecycle(db.Model):
+    __tablename__ = 'lifecycle'
+    lifecycle_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    contract_id = db.Column(db.Integer, db.ForeignKey('contract.contract_id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.service_id'), nullable=False)
+    effective_date = db.Column(db.Date)
+    invoice_Date = db.Column(db.Date)
+    invoice_amount = db.Column(db.Numeric(10,2), db.CheckContrainst('invoice_amount > 0'))
+    service_term = db.Column(db.Integer, nullable=False)
+    service_term_date = db.Column(db.Date)
+    service_term_end = db.Column(db.Date)
+    lifecycle_type = db.Column(db.Text, nullable=False)
+
+    customer = db.relationship('Customer', backref='lifecycle')
+    service = db.relationship('Service', backref='lifecycle')
+    contracts = db.relationship('Contract', backref='lifecycle')
+
+    def __repr__(self):
+        return f'<Lifecycle id: {self.lifecycle_id} Contract id: {self.contract_id}, Customer id: {self.customer_id}, Service id: {self.service_id}>'
+    
+class QBData(db.Model):
+    __tablename__ = 'qb_data'
+    event_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.service_id'), nullable=False)
+    contract_id = db.Column(db.Integer, db.ForeignKey('contract.contract_id'), nullable=False)
+    date = db.Column(db.Date)
+    service_amount = db.Column(db.Numeric(10,2))
+    balance = db.Column(db.Numeric(10,2))
+
+    customer = db.relationship('Customer', backref='QBData')
+    service = db.relationship('Service', backref='QBData')
+    contract = db.relationship('Contract', backref='QBData')
+
+    def __repr__(self):
+        return f'<Event id: {self.lifecycle_id} Contract id: {self.contract_id}, Customer id: {self.customer_id}, Service id: {self.service_id}>'
